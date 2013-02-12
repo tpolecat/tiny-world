02bb43886ebd985be0ad40c93505b5d1f1c6562e
Rob Norris <rob_norris@mac.com>
2013-02-11 17:13:43 -0800

    added JMapWorld example

---
+++ src/test/scala/org/tpolecat/tiny/world/example/JMapWorld.scala
@@ -0,0 +1,59 @@
+package org.tpolecat.tiny.world.example
+
+import org.tpolecat.tiny.world.PublicWorld
+
+// Use case: some API gives us a Java map and we're supposed to query and 
+// mutate it. How can we do this in a pure way?
+
+// First we need to construct an effect world that understands how to manipulate 
+// Java maps. We want to pass in an existing map, so the world is public.
+trait JMapWorld[K, V] extends PublicWorld {
+
+  // The type of thing we're working with
+  type State = java.util.Map[K, V]
+
+  // Primitive actions; typically constructed using effect(...)
+  def get(k: K): Action[Option[V]] = effect(_.get(k)).map(Option(_))
+  def put(k: K, v: V): Action[Option[V]] = effect(_.put(k, v)).map(Option(_))
+  def clear():Action[Unit] = effect(_.clear())
+
+  // A derived action. Note the use of the unit(...) constructor.
+  def modify(k: K, f: V => V):Action[Unit] = for {
+    v <- get(k)
+    _ <- v.map(f).map(put(k, _)).getOrElse(unit())
+  } yield ()
+
+}
+
+// What does this look like to the consumer?
+object JMapWorldTest extends App {
+
+  // Let's create a Java map with something in it. This is just setup; in real
+  // life this is the object that gets handed to us.
+  val m = new java.util.HashMap[String, String]
+  m.put("foo", "bar")
+  println("Initial map state is: " + m)
+
+  // Create an effect world that knows about maps. JMapWorld does this out of
+  // the box, but we need to provide type arguments. The world is simply a
+  // module that exposes a set of actions we can use.
+  object MyWorld extends JMapWorld[String, String]
+  import MyWorld._
+
+  // Here is some code that updates and queries a map using world actions. This
+  // code is entirely pure; it's a value in fact.
+  val action = for {
+    f <- get("foo")
+    _ <- clear()
+    _ <- put("bar", "qux")
+    _ <- modify("bar", _ + "abc")
+  } yield "foo was %s".format(f)
+
+  // The impurity is pushed into a single invocation, which we will call out.
+  val result = action.run(m) // CAREFUL HERE
+
+  // What have we done?
+  println("Result was: " + result)
+  println("New map state is: " + m)
+  
+}
