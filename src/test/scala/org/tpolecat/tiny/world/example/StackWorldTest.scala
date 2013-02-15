b190efb91140dcbc9ed05d4fdcdaf5d34584ce1a
Rob Norris <rob_norris@mac.com>
2013-02-14 20:00:42 -0800

    added stack-blowing example

--- src/test/scala/org/tpolecat/tiny/world/example/StackWorldTest.scala
+++ src/test/scala/org/tpolecat/tiny/world/example/StackWorldTest.scala
@@ -1,5 +1,6 @@
 package org.tpolecat.tiny.world.example
 
+import scalaz.syntax.monad._
 import org.tpolecat.tiny.world.StackWorld
 
 object StackWorldTest extends App {
@@ -22,6 +23,20 @@ object StackWorldTest extends App {
 
   println(action.eval(collection.mutable.Map("foo" -> 33)))
     
+  // Recursive actions can blow the stack. This isn't a problem for worlds
+  // based on the free monad.
+  val recur:Action[String] = for {
+    a <- get("foo")
+    _ <- a match {
+      case Some(n) if n > 0 => put("foo", n - 1) >> recur
+      case _ => nop
+    }
+  } yield "done"
+  
+  println(recur.eval(collection.mutable.Map("foo" -> 10))) // ok
+  println(recur.eval(collection.mutable.Map("foo" -> 10000))) // blows the stack
+  
+  
 }
 
 
